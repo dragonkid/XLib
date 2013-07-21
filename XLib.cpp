c650b4a3fa056ef0f8540813c6fef177acfc3c03
dragonkid <idragonkid@gmail.com>
2013-07-22 05:20:20 +0800

    first commit

---
+++ XLib.cpp
@@ -0,0 +1,20 @@
+#include "XLib.h"
+
+bool startWith(const std::string in_str, const std::string in_strFlag)
+{
+	
+}
+
+bool endWith(const std::string in_str, const std::string in_strFlag)
+{
+	
+}
+
+void spliteStr2Vec(
+	const std::string in_str, 
+	const std::string in_strFlag, 
+	std::vector<std::string>& out_vec
+	)
+{
+
+}
---
+++ XLib.h
@@ -0,0 +1,7 @@
+#ifndef XLIB_H
+#define XLIB_H
+
+#include <vector>
+#include <string>
+
+#endif
