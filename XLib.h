9ba8ab1780b51c50664b4cf7003688bb8a18d80e
dragonkid <idragonkid@gmail.com>
2013-07-23 23:54:48 +0800

    complete startWith & endWith
    tested

--- XLib.h
+++ XLib.h
@@ -4,4 +4,43 @@
 #include <vector>
 #include <string>
 
+namespace XLib
+{
+// function:	get whether in_str is start with in_strFlag
+// in:			in_str, in_strFlag
+// out:			nop
+// return:		bool
+//				true:	start with
+//				false:	not start with
+// complete:	2013-7-23
+// examples:	("test", "test")	=>	true
+//				("test123", "test") =>	true
+//				("test123", "st1")	=>	false
+//				("test", "")		=>	true
+//				("", "")			=>	true
+//				("123", "1234")		=>	false
+bool startWith(const std::string in_str, const std::string in_strFlag);
+
+// function:	get whether in_str is end with in_strFlag
+// in:			in_str, in_strFlag
+// out:			nop
+// return:		bool
+//				true:	end with
+//				false:	not end with
+// complete:	2013-7-23
+// examples:	("test", "test")	=>	true
+//				("123test", "test")	=>	true
+//				("123test", "st1")	=>	false
+//				("test", "")		=>	true
+//				("", "")			=>	true
+//				("123", "1234")		=>	false
+bool endWith(const std::string in_str, const std::string in_strFlag);
+
+// incomplete
+void spliteStr2Vec(
+	const std::string in_str, 
+	const std::string in_strFlag, 
+	std::vector<std::string>& out_vec
+	);
+}
 #endif
--- main.cpp
+++ main.cpp
@@ -3,5 +3,12 @@
 
 int main()
 {
+	std::string tmp_str = "123";
+	std::string tmp_strFlag = "1234";
+
+	bool tmp_b = XLib::endWith(tmp_str, tmp_strFlag);
+
+	std::cout << tmp_b << std::endl;
+
 	return 0;
 }
